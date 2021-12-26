c11de8d8355bea6a36f242e196901f198813d0dc
cloer <chltjrrb97@gmail.com>
2021-12-26 15:46:58 +0900

    ADD: bullets

--- shooting-game.js
+++ shooting-game.js
@@ -1,9 +1,8 @@
 "use strict";
 const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-const c = canvas.getContext("2d");
 
 class Circle {
     constructor(x, y, r, color) {
@@ -20,6 +19,9 @@ class Circle {
     }
 }
 
+// player setting
+const playerSize = 20;
+const playerColor = "white";
 class Player extends Circle {
     constructor(x, y, r, color) {
         super(x, y, r, color);
@@ -27,10 +29,59 @@ class Player extends Circle {
     }
 }
 
+// bullet setting
+const bulletSpped = 6;
+const bulletSize = 5;
+const bulletColor = "white";
+class Bullet extends Circle {
+    constructor(x, y, r, color, directionVector) {
+        super(x, y, r, color);
+        this.dV = directionVector;
+        this.speed = bulletSpped;
+    }
+    update() {
+        this.x += this.speed * this.dV.x;
+        this.y += this.speed * this.dV.y;
+        this.draw();
+    }
+}
+
 // Player instance
 const playerLocX = canvas.width / 2;
 const playerLocY = canvas.height / 2;
-const playerSize = 10;
-const playerColor = "white";
 const player = new Player(playerLocX, playerLocY, playerSize, playerColor);
-player.draw();
+
+// Bullets instance
+const bullets = [];
+
+const backgroundColor = "rgba(0,0,0,0.1)";
+const animate = () => {
+    requestAnimationFrame(animate);
+
+    // background
+    c.fillStyle = backgroundColor;
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    // player
+    player.draw();
+
+    // bullets
+    bullets.forEach((bullet, bulletIdx) => {
+        bullet.update();
+    });
+};
+animate();
+
+const getDirection = (fromX, fromY, toX, toY) => {
+    const angle = Math.atan2(toY - fromY, toX - fromX);
+    return {
+        x: Math.cos(angle),
+        y: Math.sin(angle),
+    };
+};
+
+window.addEventListener("click", (e) => {
+    const directionVector = getDirection(playerLocX, playerLocY, e.clientX, e.clientY);
+    const bullet = new Bullet(playerLocX, playerLocY, bulletSize, bulletColor, directionVector);
+    bullets.push(bullet);
+});
