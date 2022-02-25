5a718d14324f88ecd6795e640290405ea56bfabb
cloer <chltjrrb97@gmail.com>
2022-02-25 01:19:03 -0500

    FIX: try-catch, send->json

--- server/src/login/loginController.js
+++ server/src/login/loginController.js
@@ -1,9 +1,9 @@
-"use strick";
+'use strick';
 
-const sanitizeHTML = require("sanitize-html");
+const sanitizeHTML = require('sanitize-html');
 
-const { response, errResponse, Message } = require("../lib/responseMessage");
-const loginProvider = require("./loginProvider");
+const { response, errResponse, Message } = require('../lib/responseMessage');
+const loginProvider = require('./loginProvider');
 
 const login = async function (req, res) {
   const { nickname, password, score } = req.body;
@@ -12,37 +12,37 @@ const login = async function (req, res) {
   const score0 = parseInt(score);
 
   // validation
-  if (!nickname0) return res.send(errResponse(Message.EMPTY_NICKNAME));
-  if (!password0) return res.send(errResponse(Message.EMPTY_PASSWORD));
-  if (nickname0.length < 4 || nickname0.length > 8) return res.send(errResponse(Message.LENGTH_NICKNAME));
-  if (password0.length < 4 || password0.length > 10) return res.send(errResponse(Message.LENGTH_PASSWORD));
-  if (!(score0 > 0)) return res.send(errResponse(Message.NAN_SCORE));
+  if (!nickname0) return res.json(errResponse(Message.EMPTY_NICKNAME));
+  if (!password0) return res.json(errResponse(Message.EMPTY_PASSWORD));
+  if (nickname0.length < 4 || nickname0.length > 8) return res.json(errResponse(Message.LENGTH_NICKNAME));
+  if (password0.length < 4 || password0.length > 10) return res.json(errResponse(Message.LENGTH_PASSWORD));
+  if (!(score0 > 0)) return res.json(errResponse(Message.NAN_SCORE));
 
   try {
     // If the nickname doen't exist save the new user's record.
     const doesNicknameExistForNo = await loginProvider.checkNicknameForNo(nickname0);
     if (!doesNicknameExistForNo) {
       const createRecordResult = await loginProvider.createRecord(nickname0, password0, score0);
-      return res.send(response(Message.SUCCESS_CREATE, { insertId: createRecordResult }));
+      return res.json(response(Message.SUCCESS_CREATE, { insertId: createRecordResult }));
     }
 
     // If the password isn't that user's password send an erorr message
     const no = doesNicknameExistForNo.no;
     const doesPasswordCorrectForScore = await loginProvider.checkPasswordForScore(no, password0);
-    if (!doesPasswordCorrectForScore) return res.send(errResponse(Message.NOT_MATCHED_PASSWORD));
+    if (!doesPasswordCorrectForScore) return res.json(errResponse(Message.NOT_MATCHED_PASSWORD));
 
     // If a new score is higher than saved score then update the record
     const dbScore = doesPasswordCorrectForScore.score;
     if (score0 > dbScore) {
       const updateRecordResult = await loginProvider.updateRecord(no, score0);
-      return res.send(response(Message.SUCCESS_UPDATE, { savedScore: dbScore, newScore: score0 }));
+      return res.json(response(Message.SUCCESS_UPDATE, { savedScore: dbScore, newScore: score0 }));
     }
-    return res.send(response(Message.SUCCESS_LOW_SCORE, { savedScore: dbScore }));
+    return res.json(response(Message.SUCCESS_LOW_SCORE, { savedScore: dbScore }));
 
     //TODO: redirection -> [GET] /ranks
   } catch (error) {
     console.log(error);
-    return res.send(errResponse(Message.SERVER_ERROR));
+    return res.json(errResponse(Message.SERVER_ERROR));
   }
 };
 
--- server/src/scoreBoard/rankController.js
+++ server/src/scoreBoard/rankController.js
@@ -1,26 +1,36 @@
-"use strick";
+'use strick';
 
-const { response, errResponse, Message } = require("../lib/responseMessage");
-const rankProvider = require("./rankProvider");
+const { response, errResponse, Message } = require('../lib/responseMessage');
+const rankProvider = require('./rankProvider');
 
 const getScoreBoard = async function (req, res) {
-  const top10List = await rankProvider.getTop10List(); //FIXME: async try-catch
-  if (top10List.length < 1) return res.send(errResponse(Message.DB_NO_RECORD_ERROR));
-  return res.send(response(Message.SUCCESS_READ, top10List));
+  try {
+    const top10List = await rankProvider.getTop10List();
+    if (top10List.length < 1) return res.json(errResponse(Message.DB_NO_RECORD_ERROR));
+    return res.json(response(Message.SUCCESS_READ, top10List));
+  } catch (error) {
+    console.log(error);
+    return res.json(errResponse(Message.SERVER_ERROR));
+  }
 };
 const getMyRank = async function (req, res) {
-  const nickname = req.params.nickname;
-  if (!nickname) return res.send(errResponse(Message.EMPTY_NICKNAME));
-  if (nickname.length < 4 || nickname.length > 8) return res.send(errResponse(Message.LENGTH_NICKNAME));
+  try {
+    const nickname = req.params.nickname;
+    if (!nickname) return res.json(errResponse(Message.EMPTY_NICKNAME));
+    if (nickname.length < 4 || nickname.length > 8) return res.json(errResponse(Message.LENGTH_NICKNAME));
 
-  const doesNicknameExistForRankingScore = await rankProvider.checkNicknameForRankingScore(nickname); //FIXME: async try-catch
-  if (!doesNicknameExistForRankingScore) return res.send(errResponse(Message.NOT_MATCHED_NICKNAEM));
-  const getMyRankresult = {
-    ranking: doesNicknameExistForRankingScore.ranking,
-    nickname,
-    score: doesNicknameExistForRankingScore.score,
-  };
-  return res.send(response(Message.SUCCESS_READ, getMyRankresult));
+    const doesNicknameExistForRankingScore = await rankProvider.checkNicknameForRankingScore(nickname);
+    if (!doesNicknameExistForRankingScore) return res.json(errResponse(Message.NOT_MATCHED_NICKNAEM));
+    const getMyRankresult = {
+      ranking: doesNicknameExistForRankingScore.ranking,
+      nickname,
+      score: doesNicknameExistForRankingScore.score,
+    };
+    return res.json(response(Message.SUCCESS_READ, getMyRankresult));
+  } catch (error) {
+    console.log(error);
+    return res.json(errResponse(Message.SERVER_ERROR));
+  }
 };
 module.exports = {
   getScoreBoard,
